3d9b224a7df3ffd1e5667d0429ca013120d1ec6f
manlikexavy <chibokaxavier@gmail.com>
2024-06-19 18:25:32 +0100

    GET ALL TASKS,GET SINGLE TASK,POST REQUEST API ALL WORKING

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,32 @@
-const getAllTask = (req, res) => {
-  res.send("get tasks");
+const Task = require("../models/Tasks");
+const getAllTask = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `Task with ${taskID} not found` });
+    } else {
+      res.status(200).json({ task });
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 const updateTask = (req, res) => {
   res.send("update Task");
---
+++ models/Tasks.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const TaskSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Must provide name"],
+      trim: true,
+      maxlength: [20, "Characters cant exceed 20"],
+    },
+    completed: { type: Boolean, default: false },
+  },
+  { timestamps: true }  
+);
+
+const Task = mongoose.model("Task", TaskSchema);
+module.exports = Task;
