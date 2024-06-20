01ef0b6ed95c84e9230bf444f425127b84c8efbf
manlikexavy <chibokaxavier@gmail.com>
2024-06-20 17:33:46 +0100

    CRUD ENDPOINTS ALL FUNCTIONAL NOW

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -28,11 +28,34 @@ const getTask = async (req, res) => {
     res.status(500).json({ error });
   }
 };
-const updateTask = (req, res) => {
-  res.send("update Task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `Task with id: ${taskID} not found` });
+    } else {
+      res.status(200).json({ task });
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `Task with id: ${taskID} not found` });
+    } else {
+      res.status(200).json({ task });
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 module.exports = {
   getAllTask,
